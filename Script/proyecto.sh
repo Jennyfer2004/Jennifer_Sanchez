4a80ada388ab42120abc843115ec69af7e04166b
Jennyfer2004 <jennifer.ss2004@gmail.com>
2023-07-25 01:16:02 -0400

    ya

--- Script/proyecto.sh
+++ Script/proyecto.sh
@@ -1,25 +1,55 @@
 #!/bin/bash
 
-latexmk -pdf informe.tex
-latexmk -pdf presentacion.tex
+read -p "Escriba la opción deseada de las siguientes:
+    run
+    slides
+    report
+    show_report
+    show_slides
+    clean
+    " option
+case $option in
+ report)
+  cd "../Informe"
+  latexmk -pdf informe.tex 
+  ;;
 
+ slides)
+  cd "../Presentación"
+  latexmk -pdf presentacion.tex
+  ;;
 
+ clean)
+  cd "../Informe"
+  rm informe.aux
+  rm *.log
+  rm *.toc
+  rm *.out
+  rm *.synctex.gz
+  rm *.fdb_latexmk
+  rm *.fls
+  rm *.nav
+  rm *.snm 
+   cd "../Presentación"
+  rm informe.aux
+  rm *.log
+  rm *.toc
+  rm *.out
+  rm *.synctex.gz
+  rm *.fdb_latexmk
+  rm *.fls
+  rm *.nav
+  rm *.snm 
+  ;;
 
-
-
-folder="./"
-rm "$folder"/*.aux
-rm "$folder"/*.log
-rm "$folder"/*.toc
-rm "$folder"/*.out
-rm "$folder"/*.synctex.gz
-rm "$folder"/*.fdb_latexmk
-rm "$folder"/*.fls
-rm "$folder"/*.nav
-rm "$folder"/*.snm
-
-
-jupyter nbconvert --execute jennifer_sanchez.ipybn
-
-xpdf presentacion.pdf
-xpdf informe.pdf
+ run)
+  cd ".."
+  jupyter nbconvert --execute jennifer_sanchez.ipybn
+  ;;
+ show_slides)
+  xpdf presentacion.pdf 
+  ;;
+ show_report)
+  xpdf informe.pdf 
+  ;;
+esac   
